6386cfe3788b2c88b0deb43f6007f467c0c39e3d
Naveen <naveenreddyp9580@gmail.com>
2021-12-21 16:37:47 +0530

    Assigment-4-Commit-1

---
+++ Assignment-4/code/src/Postview.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import './Postview.css';
+import icon from './images/icon.png';
+import camera from './images/camera.png';
+import like from './images/heart.png';
+import share from './images/send.png';
+
+
+
+const Postview=()=> {
+  // debugger
+  const [users,userData]=useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/userData")
+    .then(response=>userData(response.data))
+  },[])
+
+  return (
+    <div className="site-container">
+      <div className='heading'>
+        <img className='icon' src={icon} alt='icon'/>
+        <h1>Insta Clone</h1>
+        <img className='camera' src={camera} alt='camera'/>
+      </div>
+      <div className="body">
+        {users && users.map((user)=>(
+          <div className='post'>
+            <div className='top'>
+              <div className='namelocation'>
+                <div className='name'>{user.name}</div>
+                <div className='location'>{user.location}</div>
+              </div>
+              <div className='dots'>...</div>
+            </div>
+            <img className='postimage' src={user.PostImage} alt="post" />
+            <div className='bottom'>
+              <div className='buttons'>
+                <img className='like' src={like} alt='like'/> 
+                <img className='share' src={share} alt='share'/>
+                <div className='date'>{user.date}</div>
+              </div>
+              <div className='likes'>{user.likes} likes</div>
+              <div className='description'>{user.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Postview;
