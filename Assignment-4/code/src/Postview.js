2b5ba8432ba729ac24c690872d533d7f2de0c8a5
Naveen <naveenreddyp9580@gmail.com>
2021-12-21 16:45:58 +0530

    Assignment-4

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -8,11 +8,23 @@ import share from './images/send.png';
 
 
 const Postview=()=> {
-  const [users,userData]=useState([])
-  useEffect(()=>{
-    fetch("http://localhost:3000/userData")
-    .then(response=>userData(response.data))
-  },[])
+  const [users, userData] = useState();
+  // const [users,userData]=useState([])
+  // useEffect(()=>{
+  //   fetch("http://localhost:3000/userData")
+  //   .then(response=>userData(response.data))
+  // },[])
+
+  useEffect(() => {
+   getData();
+    async function getData() {
+      const response = await fetch('http://localhost:3000/userData');
+      const data = await response.json();
+      // jest.setTimeout(30000)
+
+      userData(data);
+    }
+  }, [])
 
   return (
     <div className="site-container">
